2c02f0e53457489da5b35e466148f8b3182614d8
Ivanstein18 <ivanstein18@yandex.ru>
2021-01-21 18:04:35 +0300

    не работает

--- Tetris/Form1.cs
+++ Tetris/Form1.cs
@@ -12,8 +12,9 @@ namespace Tetris
 {
     public partial class Form1 : Form
     {
+        Shape currentShape;
         int size;
-        int[,] map = new int[8, 16];
+        int[,] map = new int[16, 8];
         public Form1()
         {
             InitializeComponent();
@@ -28,20 +29,62 @@ namespace Tetris
         public void Init()
         {
             size = 25;
+
+            currentShape = new Shape(3, 0);
+
+            
+            timer1.Interval = 1000;
+            timer1.Tick += new EventHandler(update);
+            timer1.Start();
+
             Invalidate();
 
         }
 
-        public void DrowMap(Graphics g)
+        private void update(object sender, EventArgs e)
+        {
+            currentShape.MoveDown();
+            Marge();
+            Invalidate();
+        }
+
+        public void Marge()
+        {
+            for (int i = currentShape.y; i < currentShape.y + 3; i++)
+            {
+                for (int j = currentShape.x; j < currentShape.x + 3; j++)
+                {
+                    map[i, j] = currentShape.matrix[i - currentShape.y, j - currentShape.x];
+
+                }
+
+            }
+        }
+
+        public void DrowMap(Graphics e)
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (map[i,j] == 1)
+                    {
+                        e.FillRectangle(Brushes.Red, new Rectangle(50 + j * size, 50 + i * size, size, size));
+                    }
+                }
+            }
+        }
+
+        public void DrowGrid(Graphics g)
         {
             for (int i = 0; i <= 16; i++)
             {
-                g.DrawLine(Pens.Black, new Point(50, 50 + i * 25), new Point(50 + 8 * 25, 50 + i * 25));                
+                g.DrawLine(Pens.Black, new Point(50, 50 + i * size), new Point(50 + 8 * size, 50 + i * size));                
 
             }
             for (int i = 0; i <= 8; i++)
             {
-                g.DrawLine(Pens.Black, new Point(50 + i * 25, 50), new Point(50 + i * 25, 50 + 16 * 25));
+                g.DrawLine(Pens.Black, new Point(50 + i * size, 50), new Point(50 + i * size, 50 + 16 * size));
 
             }
 
@@ -49,7 +92,7 @@ namespace Tetris
 
         private void OnPaint(object sender, PaintEventArgs e)
         {
-            DrowMap(e.Graphics);
+            DrowGrid(e.Graphics);
         }
     }
 }
---
+++ Tetris/Shape.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class Shape
+    {
+        public int x;
+        public int y;
+        public int[,] matrix;
+
+        public Shape(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+
+            matrix = new int[3, 3]
+            {
+                {0,1,0},
+                {0,1,1},
+                {0,0,1}
+            };
+        }
+
+        public void MoveDown()
+        {
+            y++;
+        }
+        public void MoveRight()
+        {
+            x++;
+        }
+        public void MoveLeft()
+        {
+            x--;
+        }
+
+    }
+}
